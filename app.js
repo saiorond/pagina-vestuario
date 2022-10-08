bbf27bddb716c604a056cdf1120f48e5caa34ab8
Débora <d.saioron@hotmail.com>
2022-10-08 16:57:19 -0300

    função checar email

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ email.addEventListener('submit', (e) => {
 });
 
 function checarInputs() {
-    if (email == '') {
-        
+    if (email === " ") {
+        mensagemDeErro(email, "O e-mail é obrigatório");
+    } else if (!checkEmail(emailValue)) {
+        mensagemDeErro(email, "Por favor, insira um e-mail válido")
+    } else {
+        mensagemDeSucesso(email);
     }
 }
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
